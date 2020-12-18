121815ca6c98725caac00d75d3a3a78fd4125315
zhanghui18990198 <72212811+zhanghui18990198@users.noreply.github.com>
2020-12-18 18:46:53 +0800

    结课作业

---
+++ EndWork/app/src/main/java/com/example/endwork/bean/GoodsInfo.java
@@ -0,0 +1,118 @@
+package com.example.endwork.bean;
+
+import com.example.endwork.R;
+
+import java.util.ArrayList;
+
+public class GoodsInfo {
+    public long rowid; // 行号
+    public int sn; // 序号
+    public String name; // 名称
+    public String desc; // 描述
+    public float price; // 价格
+    public String thumb_path; // 小图的保存路径
+    public String pic_path; // 大图的保存路径
+    public int thumb; // 小图的资源编号
+    public int pic; // 大图的资源编号
+    public int pic_id;
+    public String title;
+    public boolean bPressed;
+    public int id;
+    private static int seq = 0;
+
+    public GoodsInfo() {
+        rowid = 0L;
+        sn = 0;
+        name = "";
+        desc = "";
+        price = 0;
+        thumb_path = "";
+        pic_path = "";
+        thumb = 0;
+        pic = 0;
+    }
+
+    // 声明一个手机商品的名称数组
+    private static String[] mNameArray = {
+            "百威", "青岛啤酒", "哈尔滨啤酒", "1664", "雪花啤酒", "乌苏啤酒","黑啤酒","崂山啤酒"
+    };
+    // 声明一个手机商品的描述数组
+    private static String[] mDescArray = {
+            "欢乐一刻，百威一刻",
+            "自然纯爽，激情共享",
+            "一口是难断，众口皆可调",
+            "幸福人生，啤酒相伴",
+            "心醉人不醉，无醇却更纯",
+            "醉了一条大江，香透整个世界",
+            "不一样的黑啤，不一样的健康",
+            "崂山啤酒，品味真自然"
+    };
+    // 声明一个手机商品的价格数组
+    private static float[] mPriceArray = {39, 34,25,35,13,35,90, 89};
+    // 声明一个手机商品的小图数组
+    private static int[] mThumbArray = {
+            R.drawable.music1, R.drawable.music2, R.drawable.music3,R.drawable.music4,
+            R.drawable.music5, R.drawable.music6, R.drawable.music7,R.drawable.music8
+    };
+    // 声明一个手机商品的大图数组
+    private static int[] mPicArray = {
+            R.drawable.m1b, R.drawable.m2b, R.drawable.m3b,R.drawable.m4b,
+            R.drawable.m5b, R.drawable.m6b, R.drawable.m7b,R.drawable.m8b
+    };
+
+    public GoodsInfo(int pic_id, String title, String desc) {
+        this.pic_id = pic_id;
+        this.title = title;
+        this.desc = desc;
+        this.bPressed = false;
+        this.id = this.seq;
+        this.seq++;
+
+    }
+
+    // 获取默认的手机信息列表
+    public static ArrayList<GoodsInfo> getDefaultList() {
+        ArrayList<GoodsInfo> goodsList = new ArrayList<GoodsInfo>();
+        for (int i = 0; i < mNameArray.length; i++) {
+            GoodsInfo info = new GoodsInfo();
+            info.name = mNameArray[i];
+            info.desc = mDescArray[i];
+            info.price = mPriceArray[i];
+            info.thumb = mThumbArray[i];
+            info.pic = mPicArray[i];
+            goodsList.add(info);
+        }
+        return goodsList;
+    }
+    private static int[] gridImageArray = {R.drawable.pic_01, R.drawable.pic_02, R.drawable.pic_03
+            , R.drawable.pic_04, R.drawable.pic_05, R.drawable.pic_06, R.drawable.pic_07
+            , R.drawable.pic_08, R.drawable.pic_09, R.drawable.pic_10};
+    private static String[] gridTitleArray = {"商场", "超市", "百货", "便利店",
+            "地摊", "食杂店", "饭店", "餐厅", "会所", "菜市场"};
+
+    public static ArrayList<GoodsInfo> getDefaultGrid() {
+        ArrayList<GoodsInfo> gridArray = new ArrayList<GoodsInfo>();
+        for (int i = 0; i < gridImageArray.length; i++) {
+            gridArray.add(new GoodsInfo(gridImageArray[i], gridTitleArray[i], null));
+        }
+        return gridArray;
+    }
+    private static int[] stagImageArray = {R.drawable.skirt01, R.drawable.skirt02, R.drawable.skirt03
+            , R.drawable.skirt04, R.drawable.skirt05, R.drawable.skirt06, R.drawable.skirt07
+            , R.drawable.skirt08, R.drawable.skirt09, R.drawable.skirt10, R.drawable.skirt11
+            , R.drawable.skirt12, R.drawable.skirt13, R.drawable.skirt14, R.drawable.skirt15
+            , R.drawable.skirt16, R.drawable.skirt17, R.drawable.skirt18, R.drawable.skirt19
+            , R.drawable.skirt20, R.drawable.skirt21, R.drawable.skirt22, R.drawable.skirt23};
+    private static String[] stagTitleArray = {"促销价", "惊爆价", "跳楼价", "白菜价", "清仓价", "割肉价",
+            "实惠价", "一口价", "满意价", "打折价", "腰斩价", "无人问津", "算了吧", "大声点",
+            "嘘嘘", "嗯嗯", "呼呼", "呵呵", "哈哈", "嘿嘿", "嘻嘻", "嗷嗷", "喔喔"};
+
+    public static ArrayList<GoodsInfo> getDefaultStag() {
+
+        ArrayList<GoodsInfo> stagArray = new ArrayList<GoodsInfo>();
+        for (int i = 0; i < stagImageArray.length; i++) {
+            stagArray.add(new GoodsInfo(stagImageArray[i], stagTitleArray[i], null));
+        }
+        return stagArray;
+    }
+}
---
+++ EndWork/app/src/main/java/com/example/endwork/bean/UserInfo.java
@@ -0,0 +1,20 @@
+package com.example.endwork.bean;
+
+public class UserInfo {
+    public long rowid;
+    public String name;
+    public String update_time;
+    public String phone;
+    public String pwd;
+
+
+    public UserInfo() {
+        rowid = 0L;
+        name = "";
+        update_time = "";
+        phone = "";
+        pwd = "";
+
+
+    }
+}
